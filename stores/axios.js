854f8f1cc4dd30609c00fe672e834fac4f064aaf
“csyen” <cs.yen0106@gmail.com>
2024-10-06 18:12:57 +0800

    update url

--- stores/axios.js
+++ stores/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const app_url = import.meta.env.VITE_API_URL
+const app_url = import.meta.env.VITE_API_URL || 'https://content.ufanews.com/'
 
 export const httpService = axios.create({
   baseURL: app_url,
