ec1b954206e67e0c5e268bff8e3e4a2baeaa2172
“csyen” <cs.yen0106@gmail.com>
2024-10-11 17:55:16 +0800

    update css

--- components/MainContent.vue
+++ components/MainContent.vue
@@ -7,7 +7,15 @@
         ]" v-html="props.mainContentText">
         </p>
     </main>
-    <a id="show-more" @click="clickShowMore" tabindex="0">{{ showMore ? 'Show Less' : 'Show More' }}</a>
+    <a id="show-more" :class="showMore ? 'flip-vertical' : 'flip-vertical-transition'" @click="clickShowMore"
+        tabindex="0">
+        <!-- {{ showMore ? 'Show Less' : 'Show More' }} -->
+        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 28 28" fill="none">
+            <path
+                d="M14.8247 24.8248L22.9913 16.6582C23.2039 16.4381 23.3215 16.1434 23.3188 15.8375C23.3161 15.5316 23.1934 15.239 22.9771 15.0227C22.7608 14.8064 22.4682 14.6837 22.1623 14.681C21.8564 14.6784 21.5617 14.796 21.3417 15.0085L15.1665 21.1837L15.1665 3.66667C15.1665 3.35725 15.0436 3.0605 14.8248 2.84171C14.606 2.62292 14.3093 2.5 13.9999 2.5C13.6904 2.5 13.3937 2.62292 13.1749 2.84171C12.9561 3.0605 12.8332 3.35725 12.8332 3.66667L12.8332 21.1837L6.65802 15.0085C6.5504 14.8971 6.42166 14.8082 6.27932 14.747C6.13699 14.6859 5.98389 14.6537 5.82898 14.6524C5.67408 14.651 5.52045 14.6805 5.37707 14.7392C5.23369 14.7979 5.10343 14.8845 4.99389 14.994C4.88435 15.1036 4.79772 15.2338 4.73906 15.3772C4.6804 15.5206 4.65088 15.6742 4.65222 15.8291C4.65357 15.984 4.68576 16.1371 4.7469 16.2795C4.80804 16.4218 4.89692 16.5505 5.00835 16.6582L13.175 24.8248C13.3938 25.0436 13.6905 25.1664 13.9998 25.1664C14.3092 25.1664 14.6059 25.0436 14.8247 24.8248Z"
+                fill="#EBC76E" />
+        </svg>
+    </a>
 </template>
 
 <script setup lang="ts">
@@ -115,6 +123,15 @@ function clickShowMore() {
     font-style: normal;
     font-weight: 500;
     line-height: normal;
-    padding-top: 1rem;
+}
+
+.flip-vertical {
+    transform: rotate(180deg);
+    transition: transform 0.3s ease;
+    margin-top: 1rem;
+}
+
+.flip-vertical-transition {
+    transition: transform 0.3s ease;    
 }
 </style>
